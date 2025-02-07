0efe6fe86bff51dc46b8f1b6701fa7370742c84a
mrinal2302 <mrinalkumar.it@gmail.com>
2025-02-07 19:30:23 +0530

    Changes done for just practice add

--- ConsumerApp/com/tution/Example1.java
+++ ConsumerApp/com/tution/Example1.java
@@ -2,6 +2,9 @@ package com.tution;
 
 public class Example1 {
     public static void main(String[] args) {
+        int a=10;
+        int b =20;
+        System.out.println(a+b);
         System.out.println("Mrinal");
         System.out.println("Kumar");
     }
